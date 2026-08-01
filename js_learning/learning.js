0a0f14547e24a8e96571a2f23298ddc351d9e53f
Ryoyu Tanaka <1012tnkryy@gmail.com>
2026-08-02 00:49:14 +0900

    数値について改めて学習した。次は文字列

--- js_learning/learning.js
+++ js_learning/learning.js
@@ -1,8 +1,14 @@
 'use strict';
 
-console.log(100);
-console.log(-10);
-console.log(2.5);
-console.log(2.5e4);
-console.log(2.5e-4);
+// /*
+// JavaScriptにおける数値の表現
+// @tanaka
+// */
+
+// console.log(100);
+// console.log(-10);
+// console.log(2.5);
+// console.log(2.5e4);//2.5 * 10^4
+// console.log(2.5e-4);//2.5 * 10^-4
+
 
